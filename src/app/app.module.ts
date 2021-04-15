c142e75d49d34c7ef3f6b53eb6ef1dff3f4ac989
rupeshtiwari <roopkt@gmail.com>
2021-04-15 12:38:39 -0400

    fixing toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { DemoMaterialModule } from './mat-module';
 import { ServiceDetailComponent } from './service-detail/service-detail.component';
+import { AzureServicesComponent } from './azure-services/azure-services.component';
 
 @NgModule({
-  declarations: [	AppComponent,
-      ServiceDetailComponent
+  declarations: [		AppComponent,
+      ServiceDetailComponent,
+      AzureServicesComponent
    ],
   imports: [
     BrowserModule,
